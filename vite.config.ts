d6893dda1b40bc951552b4f1680f38fba1e21724
Denis <tomashivskuy.54@gmail.com>
2023-04-20 19:12:51 +0300

    commented server in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
       src: "/src",
     },
   },
-  server: {
-    port: 3000,
-    host: true,
-  },
+  // server: {
+  //   port: 3000,
+  //   host: true,
+  // },
 });
